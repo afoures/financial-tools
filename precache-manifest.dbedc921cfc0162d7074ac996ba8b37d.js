1fa12065edfb49f03ece284d4d8affdd1bfa6435
afoures <afoures@student.42.fr>
2020-04-02 17:34:09 +0200

    Updates

---
+++ precache-manifest.dbedc921cfc0162d7074ac996ba8b37d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93591d7d6fa1bd175a8408944b15e94",
+    "url": "/financial-tools/index.html"
+  },
+  {
+    "revision": "c0be0973e38275e90ce7",
+    "url": "/financial-tools/static/css/main.0aad48de.chunk.css"
+  },
+  {
+    "revision": "8d2b51a6b6560087bd76",
+    "url": "/financial-tools/static/js/2.d7f07907.chunk.js"
+  },
+  {
+    "revision": "0229f2113b78559ca1daeb1dd663a5a5",
+    "url": "/financial-tools/static/js/2.d7f07907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0be0973e38275e90ce7",
+    "url": "/financial-tools/static/js/main.8ccee896.chunk.js"
+  },
+  {
+    "revision": "5754ae53a95049204bc9",
+    "url": "/financial-tools/static/js/runtime-main.e244d3ec.js"
+  }
+]);
