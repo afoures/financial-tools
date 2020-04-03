ad78590ca8d021deb68495847bb8eaa501c18276
afoures <afoures@student.42.fr>
2020-04-03 18:47:43 +0200

    Updates

---
+++ precache-manifest.184bd72e396fae4028569d20955a75c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b126a95f50e340fc110fcf0610ba6f73",
+    "url": "/financial-tools/index.html"
+  },
+  {
+    "revision": "19345268d40a7bd2bab8",
+    "url": "/financial-tools/static/css/main.917d274b.chunk.css"
+  },
+  {
+    "revision": "e2831d5eacff5df1e8dd",
+    "url": "/financial-tools/static/js/2.c6068425.chunk.js"
+  },
+  {
+    "revision": "c758a309bc8a48e0297a96eac0d84f92",
+    "url": "/financial-tools/static/js/2.c6068425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19345268d40a7bd2bab8",
+    "url": "/financial-tools/static/js/main.f62b9a68.chunk.js"
+  },
+  {
+    "revision": "5754ae53a95049204bc9",
+    "url": "/financial-tools/static/js/runtime-main.e244d3ec.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/financial-tools/precache-manifest.4dbbd9d374f0293393ba052e0859d5fe.js"
+  "/financial-tools/precache-manifest.184bd72e396fae4028569d20955a75c2.js"
 );
 
 self.addEventListener('message', (event) => {
